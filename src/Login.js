532229383cce9a5a1979bc0c52162b42a474f0a8
Eiche-S <123502299+Eiche-S@users.noreply.github.com>
2023-04-25 10:38:08 +0300

    Update Login.js
    works with new modular syntax in firebase.

--- src/Login.js
+++ src/Login.js
@@ -1,69 +1,65 @@
-import React, { useState } from 'react';
-import './Login.css'
-import { Link, useHistory } from "react-router-dom";
-import { auth } from "./firebase";
+import React, { useState } from "react";
+import "./Login.css";
+import { Link, useNavigate } from "react-router-dom";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+
+import { firebaseApp } from "./firebase";
+
+const auth = getAuth(firebaseApp);
 
 function Login() {
-    const history = useHistory();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-    const signIn = e => {
-        e.preventDefault();
+  const signIn = (e) => {
+    e.preventDefault();
 
-        auth
-            .signInWithEmailAndPassword(email, password)
-            .then(auth => {
-                history.push('/')
-            })
-            .catch(error => alert(error.message))
-    }
+    signInWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        navigate("/");
+      })
+      .catch((error) => alert(error.message));
+  };
 
-    const register = e => {
-        e.preventDefault();
+  const register = (e) => {
+    e.preventDefault();
 
-        auth
-            .createUserWithEmailAndPassword(email, password)
-            .then((auth) => {
-                // it successfully created a new user with email and password
-                if (auth) {
-                    history.push('/')
-                }
-            })
-            .catch(error => alert(error.message))
-    }
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((auth) => {
+        console.log(auth);
+      })
+      .catch((error) => alert(error.message));
+  };
 
-    return (
-        <div className='login'>
-            <Link to='/'>
-                <img
-                    className="login__logo"
-                    src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png' 
-                />
-            </Link>
+  return (
+    <div className="login">
+      <Link to="/">
+        <img className="login__logo" src="./123.png" />
+      </Link>
 
-            <div className='login__container'>
-                <h1>Sign-in</h1>
+      <div className="login__container">
+        <h1>Sign-in</h1>
 
-                <form>
-                    <h5>E-mail</h5>
-                    <input type='text' value={email} onChange={e => setEmail(e.target.value)} />
+        <form>
+          <h5>E-mail</h5>
+          <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
 
-                    <h5>Password</h5>
-                    <input type='password' value={password} onChange={e => setPassword(e.target.value)} />
+          <h5>Password</h5>
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          <button type="submit" onClick={signIn} className="login__signinButton">
+            Sign In
+          </button>
+        </form>
 
-                    <button type='submit' onClick={signIn} className='login__signInButton'>Sign In</button>
-                </form>
+        <p>By signing-in you agree to Craftsmanship Baltic's Conditions of Use & Sale. Please see our Privacy Notice.</p>
 
-                <p>
-                    By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use & Sale. Please
-                    see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
-                </p>
-
-                <button onClick={register} className='login__registerButton'>Create your Amazon Account</button>
-            </div>
-        </div>
-    )
+        <button onClick={register} className="login__registerButton">
+          Create your Craftsmanship Account
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
